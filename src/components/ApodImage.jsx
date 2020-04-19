fda544e47d152aa23d04bb49f7a1a8b73bfac510
Dustin Myers <dustin@lambdaschool.com>
2020-04-19 00:35:14 -0600

    Fix warnings

--- src/components/ApodImage.jsx
+++ src/components/ApodImage.jsx
@@ -7,7 +7,7 @@ export default function ApodImage({ apod }) {
   return (
     <section className="apod-image">
       {apod.media_type === "image" ? (
-        <img src={apod.url} />
+        <img src={apod.url} alt="NASA apod" />
       ) : (
         <ReactPlayer width="100%" controls url={apod.url} playing />
       )}
